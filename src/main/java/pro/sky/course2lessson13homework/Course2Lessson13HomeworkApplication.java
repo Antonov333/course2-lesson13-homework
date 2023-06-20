bf8655607c39f4c8222f3a95245eb9bb493f3b45
Antonov333 <antonovsergei333@yahoo.com>
2023-06-20 11:33:58 +0300

    Initial commit for Course 2 Lesson 13 Homework with  shop add() and get() items methods

---
+++ src/main/java/pro/sky/course2lessson13homework/Course2Lessson13HomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.course2lessson13homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Lessson13HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2Lessson13HomeworkApplication.class, args);
+    }
+
+}
